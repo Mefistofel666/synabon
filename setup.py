d04d389b20c635f2162b8a6d2dc9f83d28a12325
Бовсуновский Александр Витальевич <avbovsuno1@mts.ru>
2023-09-24 01:53:17 +0300

    ADD: download_url

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ setup(
     author='Kirill Duvakin',
     author_email='kbduvakin1@mts.ru',
     packages=find_packages(include=['synabon']),
-    download_url=""
+    download_url="https://github.com/Mefistofel666/synabon/archive/refs/tags/v.0.1.0.tar.gz",
     install_requires=['numpy>=1.25', 'pandas>=2.1'],
     setup_requires=['pytest-runner'],
     tests_require=['pytest==7.4.2'],
