6464d181c79501bf40546c11e141ca06069a2c55
Бовсуновский Александр Витальевич <avbovsuno1@mts.ru>
2023-09-24 00:23:48 +0300

    ADD: setup.py

---
+++ setup.py
@@ -0,0 +1,15 @@
+from setuptools import find_packages, setup
+
+
+setup(
+    name='synabon',
+    version='0.1.0',
+    description='Python tool for generating data',
+    author='Kirill Duvakin',
+    author_email='kbduvakin1@mts.ru',
+    packages=find_packages(include=['synabon']),
+    install_requires=['numpy>=1.25', 'pandas>=2.1'],
+    setup_requires=['pytest-runner'],
+    tests_require=['pytest==7.4.2'],
+    test_suite='tests',
+)
