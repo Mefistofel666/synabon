0f1c6bd8e9cd146f48d86714dc30150c3aee8fe8
Бовсуновский Александр Витальевич <avbovsuno1@mts.ru>
2023-09-24 01:58:11 +0300

    FIX: setup

--- setup.py
+++ setup.py
@@ -1,14 +1,16 @@
-from setuptools import find_packages, setup
+from distutils.core import find_packages, setup
 
 
 setup(
     name='synabon',
     version='0.1.0',
+    license='MIT',
     description='Python tool for generating data',
     author='Kirill Duvakin',
     author_email='kbduvakin1@mts.ru',
     packages=find_packages(include=['synabon']),
-    download_url="https://github.com/Mefistofel666/synabon/archive/refs/tags/v.0.1.0.tar.gz",
+    url='https://github.com/Mefistofel666/synabon',
+    download_url='https://github.com/Mefistofel666/synabon/archive/refs/tags/v.0.1.0.tar.gz',
     install_requires=['numpy>=1.25', 'pandas>=2.1'],
     setup_requires=['pytest-runner'],
     tests_require=['pytest==7.4.2'],
